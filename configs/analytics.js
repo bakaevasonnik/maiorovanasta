b3866dcb14e917a69a5153c4c58a2a25e11204c2
bakaevasonnik <bakaevasonnik@yandex.ru>
2024-12-25 02:51:19 +0500

    Updated configs

--- configs/analytics.js
+++ configs/analytics.js
@@ -1,1 +1,2 @@
 Quantum Computing Virtual Machines Load Testing OAuth Performance Logging Lambda Functions Machine Learning Backend Big Data If you can dream it, you can do it. Zero Downtime Deployment Act as if what you do makes a difference. It does. Event-driven Architecture Code Review
+RabbitMQ Kafka Event-driven Architecture Python UX Optimization Containerization
